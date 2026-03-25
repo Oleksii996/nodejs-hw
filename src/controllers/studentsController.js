bee98f7ba420de3006e6940bbc966cebd2f9e1cd
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-03-25 15:48:02 +0100

    update code

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -22,3 +22,9 @@ export const getStudentById = async (req, res) => {
 
   res.status(200).json(student);
 };
+
+// Новий контролер
+export const createStudent = async (req, res) => {
+  const student = await Student.create(req.body);
+  res.status(201).json(student);
+};
