61fc4e2520f42bcbc547657ba1abaff63c94f6cd
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-03-25 01:14:36 +0100

    update code

---
+++ src/server.js
@@ -0,0 +1,62 @@
+// src/server.js
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Routes
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong, please try again'
+      : `${err.message}`,
+  });
+});
+
+// Запуск сервера - окремий пункт, щоб не блокувати інші операції
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
