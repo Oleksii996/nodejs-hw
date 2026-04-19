330f9f4f697b8c54f753768639288ad22116eeac
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-04-19 15:53:56 +0200

    update code - lesson 3 end

--- src/models/note.js
+++ src/models/note.js
@@ -3,22 +3,26 @@ import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
+    // валідація на рівні схеми для забезпечення цілісності даних
     title: {
       type: String,
       required: true,
       trim: true,
     },
+    // дозволяємо порожній рядок, але не null або undefined
     content: {
       type: String,
       default: '',
       trim: true,
     },
+    // використовуємо валідацію для перевірки id
     tag: {
       type: String,
       default: 'Todo',
       enum: TAGS,
     },
   },
+
   {
     timestamps: true,
     versionKey: false,
